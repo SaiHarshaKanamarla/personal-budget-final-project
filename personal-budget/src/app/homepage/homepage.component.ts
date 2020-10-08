fed805d13a4aa1046fef21cb65e32977cc5fcc9f
sidharsha <siddharth.landers@gmail.com>
2020-10-08 00:03:28 -0400

    Moved files to the angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js'
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+  
+  public dataSource = {
+    datasets: [{
+        data: [],
+        backgroundColor : [
+            '#ffcd56',
+            '#ff6384',
+            '#40E82F',
+            '#6E453C',
+            '#23rvfs',
+            '#6324EA',
+            '#A9256B',
+            '#0BF0E8',
+            '#F0FF00'
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        
+    ]
+};
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res: any)=>{
+      console.log(res);
+      for(var i=0;i<res.myBudget.length;i++){
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+        this.createChart();
+    }
+    })
+  }
+
+  createChart(){
+    var ctx = document.getElementById("myChart");
+    var myPieChart = new Chart(ctx,{
+        type: 'pie',
+        data : this.dataSource
+    })
+}
+
+}
