795a48dac0e0cc95d4845fc0d7ebb998f6304a6d
sidharsha <siddharth.landers@gmail.com>
2020-12-02 18:51:15 -0500

    GET request to fetch the budget data from database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const mongoose  = require('mongoose');
+const bodyParser = require('body-parser');
+const budgetModel = require('./models/budgetModel');
 const port = 3000;
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var url = 'mongodb://localhost:27017/budget-final';
 app.use('',express.static('public'));
 app.use(cors());
 
@@ -11,10 +19,26 @@ app.get('/hello',(req,res)=>{
     res.send("sample text");    
 });
 
-var sampleData = require('./sampleData.json');
+// var sampleData = require('./sampleData.json');
+
+
 app.get('/budget',(req,res)=>{
-    console.log(sampleData);
-    res.json(sampleData);    
+    // console.log(sampleData);
+    // res.json(sampleData); 
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection to the database is established");
+                budgetModel.find({})
+                           .then((data)=>{
+                               console.log(data);
+                               res.status(200).send(data);
+                               mongoose.connection.close();
+                           })
+                           .catch((err)=>{
+                               console.log(err);
+                               res.status(500).send();
+                           })
+            })   
 })
 
 
