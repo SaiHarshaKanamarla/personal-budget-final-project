6d36f90cc24578a1ed16bbd66b8b082f9ccf8ee1
sidharsha <siddharth.landers@gmail.com>
2020-12-04 19:34:45 -0500

    GET and POST requests for the feedback module

---
+++ server/models/feedbackModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+const feedbackSchema = mongoose.Schema({
+    _id:{
+        type: String,
+        required : false,
+        trim : true,        
+    },
+    username :{
+        type: String,
+        required : true,
+        trim: true,
+        unique: true
+    },
+    email :{
+        type : String,
+        required : true,
+        unique:true,
+        match:[/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,"Invalid Email Format"]        
+    },
+    description:{
+        type:String,
+        required: true,
+        trim:true,        
+    }
+},{collection : 'feedback'})
+
+const feedbackModel = mongoose.model('feedback',feedbackSchema);
+
+module.exports = feedbackModel;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose  = require('mongoose');
 const bodyParser = require('body-parser');
 const budgetModel = require('./models/budgetModel');
 const userModel = require('./models/userModel');
+const feedbackModel = require('./models/feedbackModel');
 const port = 3000;
 
 
@@ -86,6 +87,45 @@ app.post('/users',(req,res)=>{
             .then(()=>{
                 console.log("Connection to the database is established");
                 userModel.insertMany(data,(err,data)=>{
+                    if(err){
+                        console.log(err);      
+                        res.send(err);
+                        mongoose.connection.close();
+                    }else{
+                        console.log("insert successful"); 
+                        res.send(data);    
+                        mongoose.disconnect();
+                    }                    
+                })                              
+})
+})
+
+// Routes for feedback model
+app.get('/feedback',(req,res)=>{
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection to the database is established");
+                feedbackModel.find({})
+                           .then((data)=>{
+                               console.log(data);
+                               res.status(200).send(data);
+                               mongoose.connection.close();
+                           })
+                           .catch((err)=>{
+                               console.log(err);
+                               res.status(500).send();
+                           })
+            }) 
+})
+
+app.post('/feedback',(req,res)=>{
+    console.log("inside post");
+    console.log(req.body);
+    let data = {id: req.body._id, username: req.body.username, email: req.body.email, description: req.body.description}
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection to the database is established");
+                feedbackModel.insertMany(data,(err,data)=>{
                     if(err){
                         console.log(err);      
                         res.send(err);
