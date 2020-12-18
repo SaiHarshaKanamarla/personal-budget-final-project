c355313cde71b4f6f9bb83907eb843c830777b0b
sidharsha <siddharth.landers@gmail.com>
2020-12-18 16:09:48 -0500

    removing server

--- server/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://harsha-admin:mkbhd9999@personalbudget.492wy.mongodb.net/budgetdata?retryWrites=true&w=majority',{
-    useNewUrlParser:true,
-    useCreateIndex : true
-});
